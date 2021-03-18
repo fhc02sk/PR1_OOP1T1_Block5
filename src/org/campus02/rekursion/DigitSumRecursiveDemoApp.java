3a7f5f5fc0f407b20f7f0c9429d0d0b2e1feefb9
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-03-18 15:53:05 +0100

    Rekursion

--- src/org/campus02/rekursion/DigitSumRecursiveDemoApp.java
+++ src/org/campus02/rekursion/DigitSumRecursiveDemoApp.java
@@ -10,6 +10,14 @@ public class DigitSumRecursiveDemoApp {
         System.out.println("getDigitSum(1234567) = " + getDigitSum(1234567));
         System.out.println("getDigitSum(12345) = " + getDigitSum(12345));
         System.out.println("getDigitSumLimited(12345) = " + getDigitSumLimited(12345));
+
+        System.out.println("power(2,4) = " + power(2,4));
+        System.out.println("power(3,3) = " + power(3,3));
+        System.out.println("power(4,4) = " + power(4,4));
+        System.out.println("power(5,3) = " + power(5,3));
+
+        System.out.println("multiplicate(5, 4) = " + multiplicate(5, 4));
+        System.out.println("multiplicate(7, 4) = " + multiplicate(7, 4));
     }
 
     // create a recursive getDigitSum out of this code
@@ -37,14 +45,24 @@ public class DigitSumRecursiveDemoApp {
 
     public static int power(int a, int b) {
         /* calculate a exp b (a hoch b) with a recursion */
-        /* a = 2, b = 3 => 2 * 2 * 2 = 8
+        /* a = 2, b = 3 => power (2, 3) = 2 * power(2, 2) => 2 * (2 * power(2, 1)) => 2 * (2 * 2)
            a = 2, b = 4 => 2 * 2 * 2 * 2 = 16
+
+           x hoch 1 => x => Abbruchbedingung
+
          */
-        return 0;
+        if (b < 2)
+            return a;
+
+        return a * power(a, b - 1);
     }
 
     public static int multiplicate(int a, int b) {
         /* Multiplicate a * b, but just do it with + and recursion */
+
+        if (b >= 1 && a >= 1)
+            return a + multiplicate(a, b - 1); // 5 x 4 = a + a + a + a => 5 + 5 + 5 + 5 + 0
+
         return 0;
     }
 
